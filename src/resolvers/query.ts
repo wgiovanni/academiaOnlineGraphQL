658f6dbb6fe46b82398acf563610a6a5787624d3
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2020-05-06 17:32:06 -0400

    Resolvers - Queries - Información del estudiante seleccionado

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -4,7 +4,19 @@ const query : IResolvers = {
     Query: {
         students(): any {
             return database.students;
-        }
+        },
+        student(__: void, {id}): any {
+            const result = database.students.filter(student => student.id === id)[0];
+            if (result === undefined) {
+                return {
+                    id: '-1',
+                    name: `No se ha encontrado el estudiante con el ID ${id}`,
+                    email: '',
+                    courses: []
+                };
+            }
+            return result;
+        },
     }
 }
 
