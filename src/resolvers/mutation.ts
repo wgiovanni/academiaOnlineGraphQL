da28ccd783892bd19281157104d743ec1405447e
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2020-05-06 18:47:14 -0400

    SResolvers - Mutation - Añadir un curso

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,7 +1,26 @@
 import { IResolvers } from 'graphql-tools';
 import _ from 'lodash'; 
+import { database } from '../data/data.store';
 const mutation : IResolvers = {
-    
+    Mutation: {
+        courseNew(__:void, {course }): any {
+            const itemCourse = {
+                id: String(database.courses.length + 1),
+                title: course.title,
+                description: course.description,
+                clases: course.clases,
+                time: course.time,
+                level: course.level,
+                logo: course.logo,
+                path: course.path,
+                teacher: course.teacher,
+                reviews: [],
+            }
+
+            database.courses.push(itemCourse);
+            return itemCourse;
+        }
+    }
 }
 
 export default mutation;
