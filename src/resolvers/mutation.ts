fdf112f983b3669f7e1f8904013e5efd43b31f80
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2020-05-07 11:26:08 -0400

    Resolvers - Mutation - Añadir un curso - Validar para evitar cursos duplicados

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -3,7 +3,7 @@ import _ from 'lodash';
 import { database } from '../data/data.store';
 const mutation : IResolvers = {
     Mutation: {
-        courseNew(__:void, {course }): any {
+        courseNew(__:void, { course }): any {
             const itemCourse = {
                 id: String(database.courses.length + 1),
                 title: course.title,
@@ -17,8 +17,23 @@ const mutation : IResolvers = {
                 reviews: [],
             }
 
-            database.courses.push(itemCourse);
-            return itemCourse;
+            if(database.courses.filter(itemCours => itemCours.title === itemCourse.title).length === 0){
+                database.courses.push(itemCourse);
+                return itemCourse;
+            }
+
+            return {
+                id: '-1',
+                title: `El curso ya existe con este titulo ${course.title}`,
+                description: '',
+                clases: -1,
+                time: 0.0,
+                level: 'ALL',
+                logo: '',
+                path: '',
+                teacher: '',
+                reviews: [],
+            }
         }
     }
 }
